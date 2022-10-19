4768629e32229e1cbfe842220fa6294fa6262941
g_garsan@hotmail.com <g_garsan@hotmail.com>
2022-10-19 19:11:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+        connection: { reconnect: true},
+        identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN
+	},
+	channels: [ 'xus_developer' ]
+});
+
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+        console.log(process.env.TWITCH_BOT_USERNAME)
+	const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME
+        
+        if ( isNotBot) {
+                client.say(channel, `Message "${message}" was sent by ${tags.username}`)
+        }
+        // "Alca: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+});
+			
